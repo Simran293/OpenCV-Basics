7c9d52238ad95eb474b27173f174514037e96e1f
Simran293 <52887957+Simran293@users.noreply.github.com>
2020-03-15 10:17:30 +0530

    Add files via upload
    face recognition files

---
+++ script.py
@@ -0,0 +1,17 @@
+import cv2
+
+cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+cap = cv2.VideoCapture("elon.jpg")
+
+res, img = cap.read()
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+faces = cascade.detectMultiScale(gray, 1.3, 5)
+
+for (x,y,w,h) in faces:
+    cv2.rectangle(img,(x,y),(x+w,y+h),(255,255,0),2)
+
+cv2.imshow('Image',img)
+k = cv2.waitKey(0)
+cap.release()
+cv2.destroyAllWindows()
